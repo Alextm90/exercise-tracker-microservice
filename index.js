2e17d55dd37eaf4508ebfcaa782364cec148a69c
Alex Miserandino <amiserandino20@gmail.com>
2023-08-16 17:05:02 -0700

    added catch handler to then methods + added comments

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-// newest
-
 // Requirements
 const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const dns = require("dns");
 
 // add mongoose + MongDB and connect
 const mongo = require("mongodb");
@@ -33,7 +30,7 @@ const USERModel = mongoose.model("user", userSchema);
 const exerciseSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: false },
   description: { type: String, required: true, unique: false },
-  duration: { type: Number, required: true, unique: false },
+  duration: { type: Number, required: false, unique: false }, 
   date: { type: Date, required: false, unique: false },
 });
 const EModel = mongoose.model("exercise", exerciseSchema);
@@ -42,10 +39,9 @@ const EModel = mongoose.model("exercise", exerciseSchema);
 app.post("/api/users", (req, res) => {
   const username = req.body.username;
   //check for object in database
-  USERModel.findOne({ username }).then((userFound) => {
+  USERModel.findOne({ username }).then((userFound) => { 
     // if user found
     if (userFound) {
-      console.log(userFound);
       res.json({ username: userFound.username, _id: userFound._id });
     } else {
       // if user not found create new user + save
@@ -55,14 +51,18 @@ app.post("/api/users", (req, res) => {
       newUser.save();
       res.json({ username: newUser.username, _id: newUser._id.toHexString() });
     }
+  }).catch((error) => {
+    res.json({ message: "Couldn't access database" })
   });
 });
 
 // post request to exercises
 app.post("/api/users/:_id/exercises", (req, res) => {
   const id = req.params._id;
-  USERModel.findById(id).then((userFound) => {
-    if (userFound) {
+  // find user by id
+  USERModel.findById(id)
+    .then((userFound) => {
+      // check for date
       if (!req.body.date) {
         date = new Date().toDateString();
       } else {
@@ -75,6 +75,7 @@ app.post("/api/users/:_id/exercises", (req, res) => {
         date: date,
         _id: userFound._id.toHexString(),
       };
+      // create + save new exercise entry
       const newExercise = new EModel({
         username: userFound.username,
         description: req.body.description,
@@ -82,50 +83,57 @@ app.post("/api/users/:_id/exercises", (req, res) => {
         date: date,
       }).save();
       res.json(respObj);
-    } else {
-      res.json({ message: "User not found" });
-    }
-  });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.send("User not found");
+    });
 });
 
 // get request for logs
 app.get("/api/users/:_id/logs", (req, res) => {
   const { from, to, limit } = req.query;
   const id = req.params._id;
-  // find user
-  USERModel.findById(id).then((userObj) => {
-    // get all exercise objects from user
-    userToFind = userObj.username;
-    let dateObj = { username: userToFind };
-    // if there are from/to parameters
-    if (from || to) {
-      dateObj.date = {};
-    }
-    if (from) {
-      dateObj.date["$gte"] = from;
-    }
-    if (to) {
-      dateObj.date["$lte"] = to;
-    }
-   // find exercise entries
-    EModel.find(dateObj)
-      .limit(limit)
-      .then((exercises) => {
-        const entries = exercises.map((entry) => {
-          return {
-            description: entry.description,
-            duration: entry.duration,
-            date: entry.date.toDateString(),
-          };
+  // find user by id
+  USERModel.findById(id)
+    .then((userObj) => {
+      userToFind = userObj.username;
+      let dateObj = { username: userToFind };
+      // if there are from/to parameters
+      if (from || to) {
+        dateObj.date = {};
+      }
+      if (from) {
+        dateObj.date["$gte"] = from;
+      }
+      if (to) {
+        dateObj.date["$lte"] = to;
+      }
+      // find exercise entries
+      EModel.find(dateObj)
+        .limit(limit)
+        .then((exercises) => {
+          const entries = exercises.map((entry) => {
+            return {
+              description: entry.description,
+              duration: entry.duration,
+              date: entry.date.toDateString(),
+            };
+          });
+          res.json({
+            username: userToFind,
+            count: exercises.length,
+            _id: id,
+            log: entries,
+          });
+        })
+        .catch((error) => {
+          res.json({ message: "Can't access exercises for this user" });
         });
-        res.json({
-          username: userToFind,
-          count: exercises.length,
-          _id: id,
-          log: entries,
-        });
-      });
-  });
+    })
+    .catch((error) => {
+      res.send("That user doesn't exist");
+    });
 });
 
 // get request for all users
@@ -134,6 +142,9 @@ app.get("/api/users", (req, res) => {
     .sort({ _id: "desc" })
     .then((usersFound) => {
       res.json(usersFound);
+    })
+    .catch((error) => {
+      res.send("Can't access users");
     });
 });
 
